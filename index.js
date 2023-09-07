dfddf068d10e7d7cd6a7875d82b430fe70b7a716
WojciechSzw <110970008+WojciechSzw@users.noreply.github.com>
2023-09-07 23:53:08 +0200

    navbar dynamic

--- index.js
+++ index.js
@@ -2,25 +2,60 @@
 let pageActual = 0;
 const pagesCount = 3;
 let wheeling = false;
+const mainContainer = document.getElementById("main");
+const navbarLink = document.getElementsByClassName("navbar-link");
 window.addEventListener("wheel", ScrollFullPage);
 function ScrollFullPage(event) {
-    var _a, _b, _c, _d;
-    console.log(wheeling);
+    var _a, _b;
     if (!wheeling) {
+        deleteNavbarChosen(); //deleting every underline under navbar-link
         if (event.deltaY > 0 && pageActual < pagesCount - 1) {
             wheeling = true;
-            (_b = (_a = document
-                .getElementById("main")) === null || _a === void 0 ? void 0 : _a.children[pageActual + 1]) === null || _b === void 0 ? void 0 : _b.scrollIntoView({ behavior: "smooth" });
+            navbarLink[pageActual + 1].classList.add("navbar-chosen"); //adding underline
+            (_a = mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.children[pageActual + 1]) === null || _a === void 0 ? void 0 : _a.scrollIntoView({
+                behavior: "smooth",
+            }); //going to another page
             pageActual++;
-            window.setTimeout(() => (wheeling = false), 1000);
+            window.setTimeout(() => (wheeling = false), 800); //sec break from scrolling
         }
         else if (event.deltaY < 0 && pageActual > 0) {
             wheeling = true;
-            (_d = (_c = document
-                .getElementById("main")) === null || _c === void 0 ? void 0 : _c.children[pageActual - 1]) === null || _d === void 0 ? void 0 : _d.scrollIntoView({ behavior: "smooth" });
+            navbarLink[pageActual - 1].classList.add("navbar-chosen"); //adding underline
+            (_b = mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.children[pageActual - 1]) === null || _b === void 0 ? void 0 : _b.scrollIntoView({
+                behavior: "smooth",
+            }); //going to another page
             pageActual--;
-            window.setTimeout(() => (wheeling = false), 1000);
+            window.setTimeout(() => (wheeling = false), 800); //sec break from scrolling
         }
         console.log(pageActual);
+    }
+}
+function goTo(pageName) {
+    var _a, _b, _c;
+    deleteNavbarChosen();
+    switch (pageName) {
+        case "about":
+            navbarLink[0].classList.add("navbar-chosen");
+            (_a = mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.children[0]) === null || _a === void 0 ? void 0 : _a.scrollIntoView({ behavior: "smooth" });
+            pageActual = 0;
+            break;
+        case "projects":
+            navbarLink[1].classList.add("navbar-chosen");
+            (_b = mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.children[1]) === null || _b === void 0 ? void 0 : _b.scrollIntoView({ behavior: "smooth" });
+            pageActual = 1;
+            break;
+        case "contact":
+            navbarLink[2].classList.add("navbar-chosen");
+            (_c = mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.children[2]) === null || _c === void 0 ? void 0 : _c.scrollIntoView({ behavior: "smooth" });
+            pageActual = 2;
+            break;
+        default:
+    }
+}
+//deletes navbar-chosen class (underline) from every element
+function deleteNavbarChosen() {
+    for (let x = 0; x < document.getElementsByClassName("navbar-link").length; x++) {
+        document
+            .getElementsByClassName("navbar-link")[x].classList.remove("navbar-chosen");
     }
 }
--- index.ts
+++ index.ts
@@ -1,27 +1,67 @@
 let pageActual: number = 0;
 const pagesCount: number = 3;
 let wheeling: boolean = false;
+const mainContainer = document.getElementById("main");
+const navbarLink = document.getElementsByClassName("navbar-link");
 
 window.addEventListener("wheel", ScrollFullPage);
 
 function ScrollFullPage(event: WheelEvent) {
-  console.log(wheeling);
   if (!wheeling) {
+    deleteNavbarChosen(); //deleting every underline under navbar-link
+
     if (event.deltaY > 0 && pageActual < pagesCount - 1) {
       wheeling = true;
-      document
-        .getElementById("main")
-        ?.children[pageActual + 1]?.scrollIntoView({ behavior: "smooth" });
+      navbarLink[pageActual + 1].classList.add("navbar-chosen"); //adding underline
+      mainContainer?.children[pageActual + 1]?.scrollIntoView({
+        behavior: "smooth",
+      }); //going to another page
       pageActual++;
-      window.setTimeout(() => (wheeling = false), 1000);
+      window.setTimeout(() => (wheeling = false), 800); //sec break from scrolling
     } else if (event.deltaY < 0 && pageActual > 0) {
       wheeling = true;
-      document
-        .getElementById("main")
-        ?.children[pageActual - 1]?.scrollIntoView({ behavior: "smooth" });
+      navbarLink[pageActual - 1].classList.add("navbar-chosen"); //adding underline
+      mainContainer?.children[pageActual - 1]?.scrollIntoView({
+        behavior: "smooth",
+      }); //going to another page
       pageActual--;
-      window.setTimeout(() => (wheeling = false), 1000);
+      window.setTimeout(() => (wheeling = false), 800); //sec break from scrolling
     }
     console.log(pageActual);
+  }
+}
+
+function goTo(pageName: string) {
+  deleteNavbarChosen();
+  switch (pageName) {
+    case "about":
+      navbarLink[0].classList.add("navbar-chosen");
+      mainContainer?.children[0]?.scrollIntoView({ behavior: "smooth" });
+      pageActual = 0;
+      break;
+    case "projects":
+      navbarLink[1].classList.add("navbar-chosen");
+      mainContainer?.children[1]?.scrollIntoView({ behavior: "smooth" });
+      pageActual = 1;
+      break;
+    case "contact":
+      navbarLink[2].classList.add("navbar-chosen");
+      mainContainer?.children[2]?.scrollIntoView({ behavior: "smooth" });
+      pageActual = 2;
+      break;
+    default:
+  }
+}
+
+//deletes navbar-chosen class (underline) from every element
+function deleteNavbarChosen() {
+  for (
+    let x = 0;
+    x < document.getElementsByClassName("navbar-link").length;
+    x++
+  ) {
+    document
+      .getElementsByClassName("navbar-link")
+      [x].classList.remove("navbar-chosen");
   }
 }
