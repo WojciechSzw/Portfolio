0104d788cc9f9b9ccb60ec47e9bb7cbd66152f59
Wojciech Szwarc <110970008+WojciechSzw@users.noreply.github.com>
2023-09-25 19:26:38 +0200

    dsnt work

--- imgCarousel.js
+++ imgCarousel.js
@@ -1,3 +1,18 @@
 document.onkeydown = function (event) {
-    console.log(event);
+    console.log(event.key);
+    switch (event.key) {
+        case "ArrowLeft":
+            console.log("lewo");
+            break;
+        case "ArrowRight":
+            console.log("prawo");
+            spinRight();
+            break;
+    }
 };
+function spinRight() {
+    var scrollItems = document.querySelectorAll(".projects__scroll-box__item");
+    Array.from(scrollItems).forEach(function (item) {
+        console.log(item);
+    });
+}
--- imgCarousel.ts
+++ imgCarousel.ts
@@ -1,3 +1,19 @@
 document.onkeydown = function(event) {
-console.log(event)
+console.log(event.key)
+switch(event.key) {
+    case "ArrowLeft":
+        console.log("lewo")
+        break;
+    case "ArrowRight":
+        console.log("prawo")
+        spinRight();
+        break;
+}
+}
+
+function spinRight() {
+    const scrollItems = document.querySelectorAll(".projects__scroll-box__item")
+    Array.from(scrollItems).forEach(function (item){
+        console.log(item)
+    })
 }
